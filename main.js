a0d33f807364fdc96d2c5cb216044a0857c8a37f
Pamela Raiane <batista.paula.pamela@escola.pr.gov.br>
2025-07-04 09:44:22 -0300

    1

---
+++ main.js
@@ -0,0 +1,8 @@
+function inserirNome (){
+    let nomeUsuario = prompt ("Informe seu nome: ")
+    let elemento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;
+
+}
+
+inserirNome();
